4881a80392f713b05ce3ce5e9a6c3e173b3674b5
Essoko <leffyzoro55@gmail.com>
2024-09-11 15:22:29 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,19 +11,19 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "237656520674"
+global.ownernomer = "237679672107"
 global.ownername = "KgTech👽"
 global.ytname = "YT: Kermhacktools"
 global.socialm = "GitHub: Kgtech-cmt"
 global.location = "India, UP, rampur"
 
-global.ownernumber = '237656520674'  //creator number
-global.ownername = 'KgTech👽' //owner name
+global.ownernumber = '237679672107'  //creator number
+global.ownername = 'peter👽' //owner name
 global.botname = 'KERM-BUG-BOT' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
-global.author = '🦄KgTech\n\nContact: +237656520674'
+global.author = '🦄KgTech\n\nContact: +237679672107'
 
 //console view/theme
 global.themeemoji = '🪀'
